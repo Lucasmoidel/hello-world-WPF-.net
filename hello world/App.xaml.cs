4b12141d5d92480b4091f29c2b41f3e97ac012a2
Lucasmoidel <99782884+Lucasmoidel@users.noreply.github.com>
2024-06-23 07:00:00 -0700

    Add project files.

---
+++ hello world/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace hello_world
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
